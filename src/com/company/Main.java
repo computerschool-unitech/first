80b239af345800aaa7479117f26ba134c8366324
Leon <AUSemchenko@mail.ru>
2021-04-17 15:22:07 +0300

    Write first

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,10 +6,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Input x:");
+        System.out.println("Hello world");
         Scanner scanner=new Scanner(System.in);
         int x = scanner.nextInt();
-        System.out.println("x="+x);
+        System.out.println(x);
 
     }
 }
