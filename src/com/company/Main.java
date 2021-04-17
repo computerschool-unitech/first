43acff0d43d397f3916df7854378b3d42b4c74d3
SpringUser <computerschoo.org.ua@gmail.com>
2021-04-17 13:13:49 +0300

    Version2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,14 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello world");
+        Scanner scanner=new Scanner(System.in);
+        int x = scanner.nextInt();
+        System.out.println(x);
     }
 }
