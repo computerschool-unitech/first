4aea9ef8a4adc2386b6662b495c6daa010d18879
Leon <AUSemchenko@mail.ru>
2021-04-24 14:55:17 +0300

    AlinaAndrey1

--- src/com/company/Andrey/Orders.java
+++ src/com/company/Andrey/Orders.java
@@ -1,5 +1,9 @@
 package Andrey;
+import Alina.Products;
+import Taras.Customers;
+
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "orders")
@@ -10,36 +14,40 @@ public class Orders {
     @Column(name = "order_id")
     private int order_id;
 
-   /* @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "customer_id")
     Customers customer_id;
 
-    */
 
-    @Column(name = "customer_id")
+
+    /*@Column(name = "customer_id")
     protected int customer_id;
+
+     */
 
 
     @Column(name = "order_status")
     protected int order_status;
     @Column(name = "order_date")
-    protected String order_date;
+    protected Date order_date;
     @Column(name = "required_date")
-    protected String required_date;
+    protected  Date required_date;
     @Column(name = "shipped_date")
-    protected String shipped_date;
+    protected Date shipped_date;
 
 
 
 
-   /* @ManyToOne(fetch = FetchType.LAZY)
+   @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id")
-    Products product_id;
+   Products product_id;
 
-    */
 
-    @Column(name = "product_id")
+
+    /*@Column(name = "product_id")
     protected int product_id;
+
+     */
 
 
     public Orders() {
@@ -54,11 +62,11 @@ public class Orders {
         this.order_id = order_id;
     }
 
-    public int getCustomer_id() {
+    public Customers getCustomer_id() {
         return customer_id;
     }
 
-    public void setCustomer_id(int customer_id) {
+    public void setCustomer_id(Customers customer_id) {
         this.customer_id = customer_id;
     }
 
@@ -70,37 +78,38 @@ public class Orders {
         this.order_status = order_status;
     }
 
-    public String getOrder_date() {
+    public Date getOrder_date() {
         return order_date;
     }
 
-    public void setOrder_date(String order_date) {
+    public void setOrder_date(Date order_date) {
         this.order_date = order_date;
     }
 
-    public String getRequired_date() {
+    public Date getRequired_date() {
         return required_date;
     }
 
-    public void setRequired_date(String required_date) {
+    public void setRequired_date(Date required_date) {
         this.required_date = required_date;
     }
 
-    public String getShipped_date() {
+    public Date getShipped_date() {
         return shipped_date;
     }
 
-    public void setShipped_date(String shipped_date) {
+    public void setShipped_date(Date shipped_date) {
         this.shipped_date = shipped_date;
     }
 
-    public int getProduct_id() {
+    public Products getProduct_id() {
         return product_id;
     }
 
-    public void setProduct_id(int product_id) {
+    public void setProduct_id(Products product_id) {
         this.product_id = product_id;
     }
+
     @Override
     public String toString() {
         return "Orders{" +
